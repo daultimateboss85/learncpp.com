841c1b192682381ec2747f24e80442ad74349d2e
judah <judahadefuye1@outlook.com>
2026-03-11 10:27:40 +0000

    Start chap11 quiz

---
+++ chap11/quiz/question1.cpp
@@ -0,0 +1,10 @@
+/*
+1a) int 5 as numeric promotions are favored over numeric conversions
+
+1b) print() cant tell which function as it could also mean use the funtion
+with a default value of 0 provided
+
+1c) both are numeric conversions so compiler says ambiguous match
+
+
+*/
---
+++ chap11/quiz/question2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+template <typename T>
+T add(T x, T y)
+{
+    return x + y;
+}
+
+template <typename T>
+T mult(T x, int y)
+{
+    return x * y;
+}
+
+template <typename T, typename U>
+std::common_type_t<T,U> sub(T x, U y)
+{
+    return x-y;
+}
+
+
+int main()
+{
+
+    std::cout << add(2,3) << "\n";
+    std::cout << add(1.2,3.4) << "\n";
+
+    std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+    return 0;
+}
