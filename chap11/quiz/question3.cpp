51991d1dbdcc629d19c4be2e2da16b93288baeec
judah <judahadefuye1@outlook.com>
2026-03-11 11:45:41 +0000

    Finish Chap11 quiz

---
+++ chap11/quiz/question3.cpp
@@ -0,0 +1,11 @@
+/*
+output is
+1
+2
+1
+2
+
+because two functions will be instantiated one for int and one for
+double
+
+*/
---
+++ chap11/quiz/question4.cpp
@@ -0,0 +1,11 @@
+/*
+1) 11 as exact matches are preferred over template when same argument
+
+2) 2 as template can be exact
+
+3) 4 as explicit usage of template
+
+4) 2 explicit type deduction - short promoted 
+
+5) 6 template usage with automatic deduction
+*/
