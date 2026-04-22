d778ff404cbafafa14e5b0aad066ef4e3a5bc037
judah <judahadefuye1@outlook.com>
2026-04-22 14:03:20 +0100

    Finish 15.5 quiz

---
+++ chap15/15.5/question1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+template <typename T, typename U, typename V>
+class Triad
+{
+    T m_first;
+    U m_second;
+    V m_third;
+
+public:
+    Triad(const T& t,const U& u,const V& v)
+        :m_first{t}
+        ,m_second{u}
+        ,m_third{v}
+        {}
+    
+    const T& first() const { return m_first;}
+    const U& second() const { return m_second;}
+    const V& third() const { return m_third;}
+
+    void print() const;
+
+};
+
+template<typename T, typename U, typename V>
+void Triad<T,U,V>::print () const
+{
+    std::cout << "[" << m_first << ", " << m_second << ", " << m_third << "]";
+}
+
+int main()
+{
+	Triad<int, int, int> t1{ 1, 2, 3 };
+	t1.print();
+	std::cout << '\n';
+	std::cout << t1.first() << '\n';
+
+	using namespace std::literals::string_literals;
+	const Triad t2{ 1, 2.3, "Hello"s };
+	t2.print();
+	std::cout << '\n';
+
+	return 0;
+}
