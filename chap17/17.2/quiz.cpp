e1ae90b90bfea38f06ab014c51759a13bb82b350
judah <judahadefuye1@outlook.com>
2026-08-18 13:03:21 +0100

    Finish 17.2 quiz

---
+++ chap17/17.2/quiz.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <array>
+
+int main()
+{
+    constexpr std::array hello {'h', 'e', 'l', 'l', 'o'};
+    std::cout << "The length is: " << hello.size() << "\n";
+    std::cout << hello[1] << hello.at(1) << std::get<1>(hello);
+}
