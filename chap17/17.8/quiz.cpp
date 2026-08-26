99c585a3b773712c3a4ca7303b4d39cfd6d3104a
judah <judahadefuye1@outlook.com>
2026-08-26 12:36:54 +0100

    Finish 17.8 and 17.9 quiz

---
+++ chap17/17.8/quiz.cpp
@@ -0,0 +1,16 @@
+/*
+
+1) 
+Array decay is the phenomenon in cpp where an array is used in an expression and the expression receives a pointer
+to the first element of the array instead of the actual array
+
+
+2)
+To know where the end of the string is.
+
+3)
+Because you can traverse the array to determine the length. 
+
+ii) 
+
+*/
---
+++ chap17/17.9/quiz.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+/*
+same as arr is jus the pointer to the first element of the array arr[0] uses pointer arithmetic to get the element at the
+0th memory address after arr
+*/
